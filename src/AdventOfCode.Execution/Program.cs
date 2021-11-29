2096f5c4569b2173b1659813e8865c4da2a762b0
thinker227 <thinker0227@gmail.com>
2021-11-29 19:07:46 +0100

    Updated solution logging and documentation

--- src/AdventOfCode.Execution/Program.cs
+++ src/AdventOfCode.Execution/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using AdventOfCode.Execution;
 
+
+
 int day = DateTime.Now.Day;
 if (args.Length > 0) {
 	if (int.TryParse(args[0], out int result))
@@ -14,4 +16,16 @@ var solver = Runner.GetSolver(day);
 var input = Runner.GetInput(day);
 var solution = solver.Solve(input);
 
-Console.WriteLine($"Part 1:\n{solution.Part1}\n\nPart2:\n{solution.Part2}");
+writePartSolution(solution.Part1, 1);
+writePartSolution(solution.Part2, 2);
+
+
+
+static void writePartSolution(string? solution, int part) {
+	if (solution is null) return;
+
+	Console.ForegroundColor = ConsoleColor.DarkGray;
+	Console.Write($"Part {part}: ");
+	Console.ForegroundColor = ConsoleColor.White;
+	Console.WriteLine(solution);
+}
--- src/AdventOfCode.Execution/Runner.cs
+++ src/AdventOfCode.Execution/Runner.cs
@@ -6,20 +6,40 @@ using AdventOfCode.Common;
 
 namespace AdventOfCode.Execution;
 
-internal static class Runner {
+/// <summary>
+/// Contains methods for running and retrieving solutions.
+/// </summary>
+public static class Runner {
 
 	private const string inputPath = @"res\input";
 	private const string solutionsAssemblyName = "AdventOfCode.Solutions";
 
 
 
+	/// <summary>
+	/// Gets the input of a specified day.
+	/// </summary>
+	/// <param name="day">The day to get the input of.</param>
+	/// <returns>A <see cref="ReadOnlySpan{T}"/> of characters representing the input.</returns>
+	/// <exception cref="FileNotFoundException">
+	/// No input file for the specified day exists.
+	/// </exception>
 	public static ReadOnlySpan<char> GetInput(int day) {
-		var path = @$"{Directory.GetCurrentDirectory()}\{inputPath}\{day:D2}.txt";
+		var path = @$"{inputPath}\{day:D2}.txt";
 		if (!File.Exists(path))
 			throw new FileNotFoundException($"Could not find input file for day {day} - '{path}' is missing.");
 		return File.ReadAllText(path);
 	}
 
+	/// <summary>
+	/// Gets the solver for a specified day.
+	/// </summary>
+	/// <param name="day">The day to get the solver for.</param>
+	/// <returns>An <see cref="ISolver"/> for the specified day.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// None or multiple solvers are found for the specified day or
+	/// the solver for the specified day does not contains a parameterless constructor.
+	/// </exception>
 	public static ISolver GetSolver(int day) {
 		var types = GetSolverTypes();
 		var solverTypes = types
@@ -33,23 +53,21 @@ internal static class Runner {
 				string typesString = string.Join(", ", solverTypes.Select(t => $"'{t.FullName}'"));
 				message = $"Multiple solvers types for day {day} were found: {typesString}.";
 			}
-			throw new Exception(message);
+			throw new InvalidOperationException(message);
 		}
 
 		var solverType = solverTypes[0];
 		var constructor = solverType.GetConstructor(Array.Empty<Type>());
 		if (constructor is null)
-			throw new Exception($"Type '{solverType.FullName}' does not contain a parameterless constructor.");
+			throw new InvalidOperationException($"Type '{solverType.FullName}' does not contain a parameterless constructor.");
 		var instance = constructor.Invoke(Array.Empty<object>());
 		return (ISolver)instance;
 	}
-
 	private static Type[] GetSolverTypes() =>
 		Assembly.Load(solutionsAssemblyName)
 			.GetTypes()
 			.Where(IsSolverType)
 			.ToArray();
-
 	private static bool IsSolverType(Type type) {
 		if (type.GetCustomAttribute<SolverAttribute>() is null) return false;
 		return type
