e2b74ed63dbcc4dfac0d09fef50dce0a2bc9e714
thinker227 <thinker0227@gmail.com>
2021-12-01 08:21:45 +0100

    Day 1 part 1

---
+++ src/AdventOfCode.Solutions/Day01.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Solutions;
+
+[Solver(1)]
+public sealed class Day01 : ISolver {
+
+	public Solution Solve(string? input) {
+		var nums = input!
+			.Split('\n')
+			.Select(s => int.Parse(s.Trim()))
+			.ToImmutableArray();
+
+		int largerCount = 0;
+		for (int i = 0; i < nums.Length; i++) {
+			if (i == 0) continue;
+			int current = nums[i];
+			int previous = nums[i - 1];
+			if (current > previous) largerCount++;
+		}
+
+		return new(largerCount.ToString());
+	}
+
+}
