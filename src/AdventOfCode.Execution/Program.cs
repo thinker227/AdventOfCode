1b78ca0c5e89387357a4067677569cc7991dc7f6
thinker227 <thinker0227@gmail.com>
2021-12-01 21:41:01 +0100

    Added command-line input

--- src/AdventOfCode.Execution/Program.cs
+++ src/AdventOfCode.Execution/Program.cs
@@ -5,7 +5,7 @@ using AdventOfCode.Execution;
 
 
 int day = DateTime.Now.Day;
-if (args.Length > 0) {
+if (args.Length >= 1) {
 	if (int.TryParse(args[0], out int result))
 		day = result;
 	else Console.WriteLine($"Could not parse argument '{args[0]}'.");
@@ -14,7 +14,7 @@ if (day is < 1 or > 25)
 	throw new FormatException($"Invalid day {day}.");
 
 var solver = Runner.GetSolver(day, Assembly.Load("AdventOfCode.Solutions"));
-var input = Runner.GetInput(day, @"res\input");
+string input = args.Length >= 2 ? args[1] : Runner.GetInput(day, @"res\input");
 var solutionResult = Runner.RunSolver(solver, input);
 
 SolutionWriter.WriteSolution(solutionResult);
