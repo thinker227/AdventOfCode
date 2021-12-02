30ebbbb46f301369fa3a447e2564160c6659739d
thinker227 <thinker0227@gmail.com>
2021-12-02 18:16:18 +0100

    Added better command line parsing

---
+++ src/AdventOfCode.Execution/ExecutionOptions.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+
+namespace AdventOfCode.Execution;
+
+/// <summary>
+/// Command-line options determining how to handle execution.
+/// </summary>
+internal sealed class ExecutionOptions {
+
+	/// <summary>
+	/// The day of the puzzle to run.
+	/// </summary>
+	[Option('d', "day", Required = false)]
+	public int? Day { get; set; }
+	/// <summary>
+	/// The input to pass to the solver.
+	/// </summary>
+	[Option('i', "input", Required = false)]
+	public string? Input { get; set; }
+
+}
--- src/AdventOfCode.Execution/Program.cs
+++ src/AdventOfCode.Execution/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Reflection;
+using CommandLine;
 using AdventOfCode.Execution;
 
 
 
-int day = DateTime.Now.Day;
-if (args.Length >= 1) {
-	if (int.TryParse(args[0], out int result))
-		day = result;
-	else Console.WriteLine($"Could not parse argument '{args[0]}'.");
-}
+ExecutionOptions options = null!;
+Parser.Default.ParseArguments<ExecutionOptions>(args)
+	.WithParsed(e => options = e);
+
+int day = options.Day ?? DateTime.Now.Day;
 if (day is < 1 or > 25)
 	throw new FormatException($"Invalid day {day}.");
 
 var solver = Runner.GetSolver(day, Assembly.Load("AdventOfCode.Solutions"));
-string input = args.Length >= 2 ? args[1] : Runner.GetInput(solver);
+string input = options.Input ?? Runner.GetInput(solver);
 var solutionResult = Runner.RunSolver(solver, input);
 
 SolutionWriter.WriteSolution(solutionResult);
