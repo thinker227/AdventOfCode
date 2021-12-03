bd8b25057a346188abae4437499752338d90d0e6
Thinker <thinker0227@gmail.com>
2021-12-03 11:22:06 +0100

    Complete day 3 part 1

--- src/AdventOfCode.Solutions/Day03.cs
+++ src/AdventOfCode.Solutions/Day03.cs
@@ -5,23 +5,26 @@ public sealed class Day03 : ISolver {
     
     public Solution Solve(string? input) {
         var span = input!.Trim().AsSpan();
-        int width = span[..(span.IndexOf('\n') + 1)].Length;
-        int numersWidth = width - 2; // Width excluding control characters
+        int width = span[..(span.IndexOf('\n') + 1)].Length; // Width of each line
+        int bitCount = width - 2; // Amount of bits in each number (width excluding control characters)
         int length = (span!.Length + 2) / width; // Funky newline stuff
         int halfLength = length / 2;
         int[] nums = new int[width]; // Last two indicies are unused
 
-        for (int i = 0; i < span.Length; i++) {
+        for (int i = 0; i < span.Length; i++)
+            // Any character with an ascii value under 49 ('1') will result in adding 0
             nums[i % width] += span[i] / '1';
-        }
 
         ushort gamma = 0;
-        for (int i = 0; i < numersWidth; i++) {
+        for (int i = 0; i < bitCount; i++) {
             gamma <<= 1;
             if (nums[i] >= halfLength) // Assume inclusive
                 gamma |= 1;
         }
-        ushort epsilon = (ushort)(gamma ^ ushort.MaxValue);
+        var bitSize = sizeof(ushort) * 8;
+        // The largest value formed by a number with an amount of bits equal to bitCount
+        ushort largest = (ushort)(ushort.MaxValue >> (bitSize - bitCount));
+        ushort epsilon = (ushort)(gamma ^ largest);
         int result = gamma * epsilon;
 
         return new(result);
