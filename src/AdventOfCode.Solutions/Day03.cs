aa0bed760664a744cedece955f0e9f96f92e9332
thinker227 <thinker0227@gmail.com>
2021-12-05 15:15:50 +0100

    Implemented allocation-free day 3 part 1 solution

--- src/AdventOfCode.Solutions/Day03.cs
+++ src/AdventOfCode.Solutions/Day03.cs
@@ -3,35 +3,36 @@ namespace AdventOfCode.Solutions;
 [Solver(3, @"input\03.txt")]
 public sealed class Day03 : ISplitSolver {
     
-        var span = input.Trim().AsSpan();
     public Part SolvePart1(string input) {
+        var span = input.AsSpan();
         int width = span[..(span.IndexOf('\n') + 1)].Length; // Width of each line
         int bitCount = width - 2; // Amount of bits in each number (width excluding control characters)
         int length = (span!.Length + 2) / width; // Funky newline stuff
         int halfLength = length / 2;
-        int[] nums = new int[width]; // Last two indicies are unused
+        Span<int> frequencies = stackalloc int[width]; // Last two indicies are unused
 
-        for (int i = 0; i < span.Length; i++)
-            // Any character with an ascii value under 49 ('1') will result in adding 0
-            nums[i % width] += span[i] / '1';
+		for (int i = 0; i < span.Length; i++)
+			// Any character with an ascii value under 49 ('1') will result in adding 0
+			frequencies[i % width] += span[i] / '1';
 
-        ushort gamma = 0;
+        int gamma = 0;
         for (int i = 0; i < bitCount; i++) {
             gamma <<= 1;
-            if (nums[i] >= halfLength) // Assume inclusive
+            if (frequencies[i] >= halfLength) // Assume inclusive
                 gamma |= 1;
         }
-        var bitSize = sizeof(ushort) * 8;
+        var bitSize = sizeof(int) * 8;
         // The largest value formed by a number with an amount of bits equal to bitCount
-        ushort largest = (ushort)(ushort.MaxValue >> (bitSize - bitCount));
-        ushort epsilon = (ushort)(gamma ^ largest);
+        int largest = int.MaxValue >> (bitSize - bitCount);
+        int epsilon = gamma ^ largest;
         int result = gamma * epsilon;
 
 		return result;
     }
 
-	public string? SolvePart2(string input) {
-		throw new NotImplementedException();
+	public Part SolvePart2(string input) {
+		//throw new NotImplementedException();
+		return 0;
 	}
 
 }
