56c713cba3e2bf5974c51dc07f485c66e070303f
thinker227 <thinker0227@gmail.com>
2021-12-07 17:25:41 +0100

    Day 7 part 2

--- src/AdventOfCode.Solutions/Day07.cs
+++ src/AdventOfCode.Solutions/Day07.cs
@@ -15,16 +15,25 @@ public sealed class Day07 : ISolver {
 		var tasks = Enumerable.Range(min, max - min)
 			.Select(i => GetConsumption(i, nums));
 		var consumptions = Task.WhenAll(tasks).GetAwaiter().GetResult();
-		int result = consumptions.OrderBy(d => d.consuption).First().consuption;
+		int p1 = consumptions.OrderBy(c => c.Consumption).First().Consumption;
+		int p2 = consumptions.OrderBy(c => c.ActualConsumption).First().ActualConsumption;
 
-		return new(result);
+		return new(p1, p2);
 	}
 
-	private static Task<(int consuption, int distance)> GetConsumption(int distance, IEnumerable<int> nums) {
+	// Asynchronous for some reason
+	private static Task<ConsumptionInfo> GetConsumption(int position, IEnumerable<int> nums) {
 		int consumption = 0;
-		foreach (int c in nums)
-			consumption += Math.Abs(c - distance);
-		return Task.FromResult((consumption, distance));
+		int actualConsumption = 0;
+		foreach (int c in nums) {
+			int steps = Math.Abs(c - position);
+			consumption += steps;
+			actualConsumption += Enumerable.Range(0, steps + 1).Sum();
+		}
+		return Task.FromResult(new ConsumptionInfo(consumption, actualConsumption));
 	}
+
+	// Consumption is part 1, ActualConsumption is part 2
+	private readonly record struct ConsumptionInfo(int Consumption, int ActualConsumption);
 	
 }
