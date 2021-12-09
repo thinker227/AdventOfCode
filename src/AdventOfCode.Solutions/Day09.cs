88a6817dcdc8d7743e0c7489425bdc7ff421edaa
Thinker <thinker0227@gmail.com>
2021-12-09 11:34:22 +0100

    Day 9 part 2

--- src/AdventOfCode.Solutions/Day09.cs
+++ src/AdventOfCode.Solutions/Day09.cs
@@ -1,19 +1,30 @@
 //namespace AdventOfCode.Solutions;
 
 [Solver(9, @"input\09.txt")]
-public sealed class Day09 : ISolver {
+public sealed class Day09 : ISplitSolver {
 	
-	public CombinedSolution Solve(string input) {
+	public Part SolvePart1(string input) {
 		var field = input.Split('\n', StringSplitOptions.TrimEntries);
 		var points = GetLowpoints(field);
 		int result = points.Count;
 		for (int i = 0; i < points.Count; i++)
 			result += points[i].Value - '0';
 
-		return new(result);
+		return result;
+	}
+	public Part SolvePart2(string input) {
+		var field = input.Split('\n', StringSplitOptions.TrimEntries);
+		var points = GetLowpoints(field);
+		int result = GetBasins(field, points)
+			.OrderByDescending(i => i)
+			.Take(3)
+			.Aggregate((a, b) => a * b);
+
+		return result;
 	}
 
 	private static IReadOnlyList<FieldPoint> GetLowpoints(string[] fields) {
+
 		int width = fields[0].Length;
 		int height = fields.Length;
 		List<FieldPoint> points = new();
@@ -33,6 +44,44 @@ public sealed class Day09 : ISolver {
 		}
 
 		return points;
+	}
+	// Returns a collection of basin sizes
+	private static IEnumerable<int> GetBasins(string[] field, IEnumerable<FieldPoint> lowpoints) {
+		List<int> basins = new();
+		foreach (var p in lowpoints)
+			basins.Add(Floodfill(field, p.Point));
+		return basins;
+	}
+	// Returns the size of a floodfill area
+	private static int Floodfill(string[] field, Point from) {
+		Queue<Point> points = new();
+		points.Enqueue(from);
+		HashSet<Point> visited = new();
+		int width = field[0].Length;
+		int height = field.Length;
+
+		void process(Point p, Point source) {
+			if (p.X < 0 || p.X >= width || p.Y < 0 || p.Y >= height) return;
+			if (visited.Contains(p)) return;
+
+			int pValue = field[p.Y][p.X];
+			int sourceValue = field[source.Y][source.X];
+
+			if (pValue > sourceValue && pValue < '9')
+				points.Enqueue(p);
+		}
+
+		while (points.Count > 0) {
+			Point current = points.Dequeue();
+			visited.Add(current);
+
+			process(new(current.X - 1, current.Y), current);
+			process(new(current.X + 1, current.Y), current);
+			process(new(current.X, current.Y - 1), current);
+			process(new(current.X, current.Y + 1), current);
+		}
+
+		return visited.Count;
 	}
 
 
