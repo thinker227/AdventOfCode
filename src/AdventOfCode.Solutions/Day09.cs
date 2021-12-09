ee7b52e43262c3c025510cae8bfd95f84a262b37
Thinker <thinker0227@gmail.com>
2021-12-09 10:41:15 +0100

    Split lowpoints into separate method

--- src/AdventOfCode.Solutions/Day09.cs
+++ src/AdventOfCode.Solutions/Day09.cs
@@ -4,26 +4,39 @@
 public sealed class Day09 : ISolver {
 	
 	public CombinedSolution Solve(string input) {
-		var lines = input.Split('\n', StringSplitOptions.TrimEntries);
-		int width = input.IndexOf('\r');
-		int height = lines.Length;
-		int result = 0;
-		
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				char current = lines[y][x];
-				char up = y > 0 ? lines[y - 1][x] : '9';
-				char down = y < height - 1 ? lines[y + 1][x] : '9';
-				char left = x > 0 ? lines[y][x - 1] : '9';
-				char right = x < width - 1 ? lines[y][x + 1] : '9';
-
-				if (current >= up || current >= down || current >= left || current >= right)
-					continue;
-				result += current - '0' + 1;
-			}
-		}
+		var field = input.Split('\n', StringSplitOptions.TrimEntries);
+		var points = GetLowpoints(field);
+		int result = points.Count;
+		for (int i = 0; i < points.Count; i++)
+			result += points[i].Value - '0';
 
 		return new(result);
 	}
+
+	private static IReadOnlyList<FieldPoint> GetLowpoints(string[] fields) {
+		int width = fields[0].Length;
+		int height = fields.Length;
+		List<FieldPoint> points = new();
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				char current = fields[y][x];
+				char up = y > 0 ? fields[y - 1][x] : '9';
+				char down = y < height - 1 ? fields[y + 1][x] : '9';
+				char left = x > 0 ? fields[y][x - 1] : '9';
+				char right = x < width - 1 ? fields[y][x + 1] : '9';
+
+				if (current >= up || current >= down || current >= left || current >= right)
+					continue;
+				points.Add(new(new(x, y), current));
+			}
+		}
+
+		return points;
+	}
+
+
+
+	private readonly record struct FieldPoint(Point Point, char Value);
 	
 }
