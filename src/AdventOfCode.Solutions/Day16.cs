0a042fadd92f58048a32dfc89eb620d6fc3f9b70
thinker227 <thinker0227@gmail.com>
2021-12-16 18:48:26 +0100

    Day 16 part 1
    Finally

--- src/AdventOfCode.Solutions/Day16.cs
+++ src/AdventOfCode.Solutions/Day16.cs
@@ -5,10 +5,11 @@ public sealed class Day16 : ISolver {
 	
 	public CombinedSolution Solve(string input) {
 		var bytes = HexToBinary(input);
-		Packet packet = new(bytes, 0, bytes.Length * 8);
-		var subpacket = packet.GetSubpacket();
+		PacketReader reader = new(bytes);
+		var packet = reader.ParsePacket();
 
-		throw new NotImplementedException();
+		int sum = SumVersions(packet);
+		return sum;
 	}
 
 	private static byte[] HexToBinary(string hex) {
@@ -28,69 +29,101 @@ public sealed class Day16 : ISolver {
 	}
 	private static byte HexDigitToByte(char c) =>
 		(byte)(c >= 'A' ? c - 'A' + 10 : c - '0');
+	
+	private static int SumVersions(Packet root) {
+		int result = root.Version;
+		if (root.Subpackets is null) return result;
+		foreach (var packet in root.Subpackets)
+			result += SumVersions(packet);
+		return result;
+	}
 
 
 
-	private readonly struct Packet {
+	private struct PacketReader {
 
 		private readonly byte[] bytes;
-		private readonly int offset;
-		private readonly int length;
+		private int position;
 
-		public byte Version { get; }
-		public byte TypeId { get; }
-		public PacketType Type =>
-			TypeId == 4 ?
-			PacketType.Literal : PacketType.Operator;
-
-		public Packet(byte[] bytes, int offset, int length) {
+		public PacketReader(byte[] bytes) {
 			this.bytes = bytes;
-			this.offset = offset;
-			this.length = length;
-
-			Version = (byte)bytes.GetInteger(offset, 3);
-			TypeId = (byte)bytes.GetInteger(offset + 3, 3);
+			position = 0;
 		}
 
-		public ulong GetLiteral() {
-			if (Type != PacketType.Literal)
-				throw new InvalidOperationException();
+		public Packet ParsePacket() {
+			byte version = (byte)ReadInteger(3);
+			byte typeId = (byte)ReadInteger(3);
 
-			int from = offset + 6;
+			if (typeId == 4) {
+				ulong literal = ReadLiteral();
+				return new(version, typeId, literal, null);
+			}
+			else {
+				byte lengthTypeId = ReadBit();
+				int readBits = lengthTypeId == 0 ? 15 : 11;
+				int subpacketSizeOrCount = (int)ReadInteger(readBits);
+				var subpackets = lengthTypeId == 0 ?
+					ReadPacketsFromSize(subpacketSizeOrCount) :
+					ReadPacketsFromCount(subpacketSizeOrCount);
+				return new(version, typeId, null, subpackets);
+			}
+		}
+		private ulong ReadLiteral() {
 			byte leading = 1;
 			ulong result = 0;
 
-			for (int i = from; leading != 0; i += 5) {
-				leading = bytes.GetBit(i);
-				byte current = (byte)bytes.GetInteger(i + 1, 4);
+			while (leading != 0) {
+				leading = ReadBit();
 				result <<= 4;
-				result |= current;
+				result |= ReadInteger(4);
 			}
 
 			return result;
 		}
+		private IReadOnlyCollection<Packet> ReadPacketsFromSize(int size) {
+			List<Packet> packets = new();
+			int end = position + size;
 
-		private byte GetLengthTypeId() =>
-			bytes.GetBit(offset + 6);
-		private int GetSubpacketSizeLength() =>
-			GetLengthTypeId() == 0 ? 15 : 11;
-		private int GetSubpacketSize(int length) =>
-			(int)bytes.GetInteger(offset + 6 + 1, length);
-		public Packet GetSubpacket() {
-			if (Type != PacketType.Operator)
-				throw new InvalidOperationException();
-			
-			int sizeLength = GetSubpacketSizeLength();
-			int length = GetSubpacketSize(sizeLength);
-			int subpacketOffset = offset + 6 + 1 + sizeLength;
-			return new(bytes, subpacketOffset, length);
+			while (position < end) {
+				var packet = ParsePacket();
+				packets.Add(packet);
+			}
+
+			return packets;
+		}
+		private IReadOnlyCollection<Packet> ReadPacketsFromCount(int count) {
+			var packets = new Packet[count];
+
+			for (int i = 0; i < count; i++) {
+				var packet = ParsePacket();
+				packets[i] = packet;
+			}
+
+			return packets;
+		}
+
+		private static byte GetBit(byte[] bytes, int position) {
+			byte b = bytes[position / 8];
+			int byteIndex = position % 8;
+			int shift = 7 - byteIndex;
+			int shifted = b >> shift;
+			int single = shifted & 1;
+			return (byte)single;
+		}
+		private byte ReadBit() =>
+			GetBit(bytes, position++);
+		private ulong ReadInteger(int length) {
+			ulong result = 0;
+			for (int i = 0; i < length; i++) {
+				result <<= 1;
+				result |= GetBit(bytes, position + i);
+			}
+			position += length;
+			return result;
 		}
 
 	}
 
-	private enum PacketType {
-		Literal,
-		Operator
-	}
-	
+	private readonly record struct Packet(int Version, int TypeId, ulong? Literal, IReadOnlyCollection<Packet>? Subpackets);
+
 }
