6f4422758eaef6b52f1edaf52f9226135a2778a0
thinker227 <thinker0227@gmail.com>
2021-12-18 21:51:03 +0100

    Create TooLazyException.cs

---
+++ src/AdventOfCode.Common/TooLazyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventOfCode.Common;
+
+/// <summary>
+/// An exception thrown when I'm too lazy to finish a day.
+/// </summary>
+public class TooLazyException : Exception {
+
+	/// <summary>
+	/// Initializes a new <see cref="TooLazyException"/> instance.
+	/// </summary>
+	public TooLazyException() : base("I'm too lazy to finish this day.") { }
+
+}
